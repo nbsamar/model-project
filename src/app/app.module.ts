cadf8f53f1e05fa15fa7be72ab56751f89a2e055
Nayab Samar <nbsamar@gmail.com>
2018-01-02 14:58:57 +0530

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {HomepageComponent} from './homepage/homepage.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
